34f085bdc3bc547892786b7cc4d0a64bd454b1e8
Mzatlin <michael.zatlin@yahoo.com>
2020-06-06 21:07:14 -0700

    Removed old Zombie Code.

--- Training Program/Assets/Scripts/MoveCameraToPoint.cs
+++ Training Program/Assets/Scripts/MoveCameraToPoint.cs
@@ -26,39 +26,6 @@ public class MoveCameraToPoint : MonoBehaviour
         }
 
         monitor = fixationPoint.transform.parent;
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-      /*  if (Input.GetKeyDown(KeyCode.E))
-        {
-            if(isFixated == false)
-            {
-                isFixated = true;
-            }
-            else
-            {
-                isFixated = false;
-            }
-        }
-
-        if (isFixated)
-        {
-           mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, fixationPoint.position, moveSpeed * Time.deltaTime);
-        
-           // mainCam.transform.LookAt(monitor);
-            var q = Quaternion.LookRotation(screen.position - mainCam.transform.position);
-            mainCam.transform.rotation = Quaternion.RotateTowards(mainCam.transform.rotation, q, 90.0f * Time.deltaTime);
-        */
-     
-    }
-    public static void Test()
-    {
-        Debug.Log("Test Reached!");
     }
 
     public static void FixateToPoint(Transform start, Transform fixationPoint, float moveSpeed)
@@ -66,7 +33,7 @@ public class MoveCameraToPoint : MonoBehaviour
         start.transform.position = Vector3.Lerp(start.transform.position, fixationPoint.position, moveSpeed * Time.deltaTime);
 
         // mainCam.transform.LookAt(monitor);
-       // var q = Quaternion.LookRotation(fixationPoint.position - fixationPoint.transform.position);
-       // start.transform.rotation = Quaternion.RotateTowards(start.transform.rotation, q, 90.0f * Time.deltaTime);
+        var q = Quaternion.LookRotation(fixationPoint.position - fixationPoint.transform.position);
+        start.transform.rotation = Quaternion.RotateTowards(start.transform.rotation, q, 90.0f * Time.deltaTime);
     }
 }
