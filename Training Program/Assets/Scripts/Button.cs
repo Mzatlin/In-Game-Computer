919295abe5cc5c960c1c095bc59266ee478abb81
Mzatlin <michael.zatlin@yahoo.com>
2020-06-10 23:56:00 -0700

    Added a spawn mechanic for the 2d player

--- Training Program/Assets/Scripts/Button.cs
+++ Training Program/Assets/Scripts/Button.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 
 public class Button : MonoBehaviour, IClickable {
 
+    public static event Action OnClicked = delegate { };
     bool isHovering;
     public bool IsHovering  { get { return isHovering; } set { isHovering = value; } }
 
 
    public void HandleClick()
     {
-        Debug.Log("I have been clicked!");
+        OnClicked();
     }
 }
